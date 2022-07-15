352bee4c1f682b3208029a68f8293dd792c22d5f
DESKTOP-KA38J9L\rladm <rladmsqo98@gmail.com>
2022-07-15 16:35:27 +0900

    coding python make to 1 problem using while and if

--- greedy/greedy_makeTo1.py
+++ greedy/greedy_makeTo1.py
@@ -1,6 +1,16 @@
 #n, k를 공백을 기준으로 입력받음
-n, k = map(int, input().split)
+n, k = map(int, input().split())
 
+#N과 K가 주어질 떄 N이 1이 될 때 까지 1번 혹은 2번의 과정을 수행해야 하는 최소 횟수 : result
 result = 0
 
+while True:
+    while n%k == 0:
+        n=n // k
+        result += 1
+    if n == 1:
+        break
+    n -= 1
+    result+=1
 
+print(result)
