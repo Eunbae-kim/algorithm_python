2db2305d7c1c09a4c0193e4008db10b3b65a7255
DESKTOP-KA38J9L\rladm <rladmsqo98@gmail.com>
2022-07-25 12:18:25 +0900

    에라토스테네스 체 알고리즘 이용해서 소수 골라내서 출력

---
+++ algorithm_etc/are_prime_numbers.py
@@ -0,0 +1,20 @@
+# 에라토스테네스의 체 알고리즘
+import math
+n = 1000 # 2붜 1000까지 모든 수에 대해 소수 판별
+# 처음엔 모든 수가 소수인 것으로 초기화(0,1은 제외)
+array = [True for i in range(n+1)]
+
+# 에라토스테네스의 체 알고리즘
+# 2부터 n까지의 제곱근까지 모든 수를 확인하며
+for i in range(2,int(math.sqrt(n)) + 1):
+    if array[i] == True : #i가 소수인 경우
+        # i를 제외한 i의 모든 배수를 지우기
+        j = 2
+        while i*j <= n:
+            array[i*j] = False
+            j += 1
+
+#모든 소수 출력
+for i in range(2,n+1):
+    if array[i]:
+        print(i, end=' ')
